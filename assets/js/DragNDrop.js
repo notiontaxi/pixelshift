f3fd22dab8eebb9c3a0f487f9cc2086d83bb7b05
notiontaxi <florian.wokurka@gmail.com>
2013-10-25 18:44:08 +0200

    added DragNDrop.js

---
+++ assets/js/DragNDrop.js
@@ -0,0 +1,61 @@
+/*
+DragNDrop class
+
+Florian Wokurka (2013)
+https://github.com/notiontaxi
+*/
+
+"use strict"
+
+define(['js/FileProcessor'], function(FileProcessor) {
+
+  var DragNDrop, module;
+  module = function() {}
+  DragNDrop = (function(){
+  //__extends(DragNDrop, Superclass);
+// --------------------------------------
+
+
+
+  function DragNDrop(canvas, callback){
+    DragNDrop.prototype.canvas = canvas;
+    DragNDrop.prototype.fileProcessor = new FileProcessor
+    DragNDrop.prototype.dropZone = canvas.getElement()[0];
+    DragNDrop.prototype.callback = callback;
+    DragNDrop.prototype.callbackObject = canvas;
+
+    DragNDrop.prototype.bindEvents();
+
+  }
+
+
+  DragNDrop.prototype.bindEvents = function(){
+    this.dropZone.addEventListener('drop', function(event){var val = this.fileProcessor.processFile(event, this.start, this)}.bind(this), false);
+    this.dropZone.addEventListener('dragover', function(event){this.showDragOver(event, true)}.bind(this), false);
+    this.dropZone.addEventListener('dragleave', function(event){this.showDragOver(event, false)}.bind(this), false);
+  }
+
+  // pass that to be in own context again
+  DragNDrop.prototype.start = function(img, that){
+    if(!!img){     
+      img.onload = function() {
+        that.callback(img, that.callbackObject);
+      }
+    }
+  }
+
+  DragNDrop.prototype.showDragOver = function(event, onOrOff){
+    event.stopPropagation();
+    event.preventDefault();
+    this.canvas.highlight(onOrOff);
+  }
+
+
+// --------------------------------------
+    return DragNDrop
+  })()
+  return module.exports = DragNDrop
+})
+
+
+
