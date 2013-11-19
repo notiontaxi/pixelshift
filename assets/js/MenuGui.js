baf6e9f340d9c4d94c1e4791760bde625953d5f9
notiontaxi <florian.wokurka@gmail.com>
2013-11-19 11:50:09 +0100

    prevent event bubbeling for clicks in menu

--- assets/js/MenuGui.js
+++ assets/js/MenuGui.js
@@ -42,11 +42,15 @@ define(['text!templates/menu-bar.html', 'js/FileProcessor', 'js/ImageProcessor',
     // Outline button
     $("#upload-image").click(
       function(event, ui){
+        event.stopPropagation()
+        event.preventDefault()
         $('input[type="file"]').click()
       }.bind(this))
 
     $("#save-image").click(
       function(event, ui){
+        event.stopPropagation()
+        event.preventDefault()
         this.fileProcessor.saveCanvasToDisk(this.canvas.getHtmlElementCopy())
     
     }.bind(this))   
@@ -64,18 +68,24 @@ define(['text!templates/menu-bar.html', 'js/FileProcessor', 'js/ImageProcessor',
 
     $("#action-grayscale").click(
       function(event, ui){
+        event.stopPropagation()
+        event.preventDefault()
         var newImg = this.imageProcessor.processGrayscale(this.canvas.getImageData(), this.canvas.getImageWidth())
         this.canvas.putImageData(newImg)
     }.bind(this))    
 
     $("#action-bitmap").click(
       function(event, ui){
+        event.stopPropagation()
+        event.preventDefault()
         var newImg = this.imageProcessor.processThreshold(this.imageProcessor.computeThreshold(this.canvas.getImageData()) ,this.canvas.getImageData())
         this.canvas.putImageData(newImg)
     }.bind(this))  
 
     $("#action-invert").click(
       function(event, ui){
+        event.stopPropagation()
+        event.preventDefault()
         var newImg = this.imageProcessor.processInvertColors(this.canvas.getImageData(), this.canvas.getImageWidth())
         this.canvas.putImageData(newImg)
     }.bind(this))          
@@ -86,11 +96,15 @@ define(['text!templates/menu-bar.html', 'js/FileProcessor', 'js/ImageProcessor',
 
     $("#action-undo").click(
       function(event, ui){
+        event.stopPropagation()
+        event.preventDefault()
         this.canvas.undo()
     }.bind(this))    
 
     $("#action-redo").click(
       function(event, ui){
+        event.stopPropagation()
+        event.preventDefault()
         this.canvas.redo()
     }.bind(this))          
 
@@ -100,18 +114,24 @@ define(['text!templates/menu-bar.html', 'js/FileProcessor', 'js/ImageProcessor',
 
     $("#action-zoom-in").click(
       function(event, ui){
+        event.stopPropagation()
+        event.preventDefault()
         this.shownCanvas.zoomIn()
         this.updateDragBoundaries()
     }.bind(this))    
 
     $("#action-zoom-out").click(
       function(event, ui){
+        event.stopPropagation()
+        event.preventDefault()
         this.shownCanvas.zoomOut()
         this.updateDragBoundaries()
     }.bind(this))   
 
     $("#action-zoom-reset").click(
       function(event, ui){
+        event.stopPropagation()
+        event.preventDefault()
         this.shownCanvas.zoomReset()
         this.updateDragBoundaries()
     }.bind(this))              
